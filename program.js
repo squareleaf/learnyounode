7b8d1358f30a1c1cea85041f04e899841d7002a5
squareleaf <tiffany.green@wellbe.me>
2016-02-10 18:59:05 -0600

    Fix stupid mistake in code

--- program.js
+++ program.js
@@ -1,6 +1,6 @@
-array = Number(process.argv);
+array = process.argv;
 var sum = 0;
-for(i = 0; i < array.length; i++) {
-  sum = sum + i;
+for(i = 2; i < array.length; i++) {
+  sum = sum + Number(array[i]);
 }
 console.log(sum);
